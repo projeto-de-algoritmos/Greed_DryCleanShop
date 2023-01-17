57ff3bc5d5e5afce3d5232999f5f49f1d582c436
Bruno Carmo Nunes <brunocmo.nunes@gmail.com>
2023-01-16 23:21:11 -0300

    Add user input menu
    Co-authored-by: marcos-mv <vinicius_kof36@hotmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,51 @@
 #include <iostream>
 #include "../inc/Greed.h"
 
-int main() {
+int main()
+{
+    Greed roupas;
 
-    Greed opa;
+    int menu{};
 
-    opa.insertClothes();
-    opa.insertClothes();
-    opa.insertClothes();
+    system("clear");
 
-    std::cout << '\n';
-    opa.init();
+    while (true) {
+
+        std::cout << "Escolha uma opção: " << '\n';
+        std::cout << "1. Inserir conjunto de roupas" << '\n';
+        std::cout << "2. Listar pedidos " << '\n';
+        std::cout << "3. Gerar otimização" << '\n';
+
+        std::cout << "0. Sair" << '\n';
+
+        std::cin >> menu;
+
+        system("clear");
+
+        switch (menu) {
+            case 1 :
+                roupas.insertClothes();
+                break;
+            case 2 :
+                roupas.listClothes();
+                std::cin.ignore();
+                break;
+            case 3 :
+                roupas.init();
+                std::cin.ignore();
+
+                break;
+            case 0 :
+
+                return 0;
+
+            default:
+                std::cout << "Escolha invalida!" << '\n';
+                break;
+        }
+    }
+
 
     return 0;
-
 }
 
